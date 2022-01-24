48ecb2f9c9a51e7ea7f5df1c9368cb8ad40b4549
Mario Velasco Alonso <mario.velasco@bizaway.com>
2022-01-24 16:56:24 +0100

    Tailwind Working && Create htmls of entry components

--- apps/train/src/app/remote-entry/entry.component.ts
+++ apps/train/src/app/remote-entry/entry.component.ts
@@ -2,20 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bizawaynx-train-entry',
-  template: `<h2>Train Vertical</h2>
-    <ul>
-      <li><a routerLink="booking">Booking</a></li>
-      <li><a routerLink="search">Search</a></li>
-    </ul>
-    <router-outlet></router-outlet>`,
-  styles: [
-    `
-      .remote-entry {
-        background-color: #143055;
-        color: white;
-        padding: 5px;
-      }
-    `,
-  ],
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.scss'],
 })
 export class RemoteEntryComponent {}
