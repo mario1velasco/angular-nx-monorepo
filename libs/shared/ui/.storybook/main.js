2982caa734de6f39d30df94896d28dd7b16002cb
Mario Velasco Alonso <mario.velasco@bizaway.com>
2022-01-26 09:16:51 +0100

    Fix few small bug Watch not working due to the version

--- libs/shared/ui/.storybook/main.js
+++ libs/shared/ui/.storybook/main.js
@@ -1,6 +1,13 @@
+// WATCH
+// https://github.com/nrwl/nx/issues/8360#issuecomment-1009348005
+
 module.exports = {
   stories: ['../src/lib/components/**/*.stories.ts'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   core: {
     builder: 'webpack5',
   },
